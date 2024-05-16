820ca6e549cfbda1d68156f42bd9039072f834cd
bpmfMohammedAbdu <155623175+bpmfMohammedAbdu@users.noreply.github.com>
2024-05-16 16:54:46 +0300

    Add files via upload

---
+++ day9one.py
@@ -0,0 +1,10 @@
+def ageandname (name,age):
+    name=str (input ("inter your name"))
+    age=int (input ("inter your age")) 
+    print("__________________________")
+    print("- _-_-_-_-_-_-_-_-_-_-_-_-")
+    print(f"Hello {name}!")
+    print(f"your are {age} yers old.")
+    born_in=2023-age 
+    print(f"you was born in {born_in}")
+ageandname("yes",0)    
