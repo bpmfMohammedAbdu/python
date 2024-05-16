897b555815db9231ab766b59a1ba339c41e8941f
bpmfMohammedAbdu <155623175+bpmfMohammedAbdu@users.noreply.github.com>
2024-05-16 16:53:49 +0300

    Add files via upload

---
+++ day 2 chats.py
@@ -0,0 +1,9 @@
+A=int(input("inter the first number"))
+B=int(input("inter the second number"))
+if A>111 and A<999 and B>111 and B<999:
+    if A !=B or 0 not in A or B:
+        print ( A[-1:])
+        print(A[-1:])
+else :
+    print ( "incorect ")        
+
